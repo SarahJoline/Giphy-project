77f96af214091a1e3668eecb2723cdd82236a498
Sarah Naas <sjnaas@gmail.com>
2019-10-22 15:49:10 -0700

    input is taken and turned in to a button

--- assets/app.js
+++ assets/app.js
@@ -4,19 +4,37 @@ var topics = ["cats", "dogs", "chickens", "cows"];
 let api_key = "8M5WZp8vzT8HJsd77ntgkf5KP16OdPuc";
 
 //buttons should be made from my topics array dynamically.
-$(document).ready(function() {
+
+makingButtons();
+
+function makingButtons() {
+  $(".button-town").empty();
+
   for (var i = 0; i < topics.length; i++) {
-    $(".button-town").append(`<button>${topics[i]}</button>`);
+    var makeBtn = $("<button>");
+    makeBtn.addClass("gif-btn");
+    makeBtn.attr("data-name", topics[i]);
+    makeBtn.text(topics[i]);
+    $(".button-town").append(makeBtn);
   }
+  //   $(".button-town").append(`<button>${topics[i]}</button>`);
+}
+
+//take input of form and push it to my topics array
+$("#search").on("click", function(event) {
+  event.preventDefault();
+
+  var topic = $("#gifSearch").val();
+  console.log(topic);
+  $("#gifSearch").val("");
+  topics.push(topic);
+  console.log(topics);
+  makingButtons();
 });
 
-$(".button-town").on("click", function() {
-  console.log("testing");
-});
 //the already made buttons should search for gifs.
 //when I do a search, it gets added to my array and a button is made.
 //10 gifs are displayed per search
 //the gif rating should be displayed too.
 //each gif should be static when loaded to the page
 //when clicked it plays, and it pauses again when clicked again.
-//
