ff3a508799338fc932f7326982d105317b09da7d
Sarah Naas <sjnaas@gmail.com>
2019-10-23 23:15:57 -0700

    gifs come up as still images and can be played and paused when clicked

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,8 @@
-//an array called topics:
 var topics = ["cats", "dogs", "chickens", "cows"];
 
 let api_key = "8M5WZp8vzT8HJsd77ntgkf5KP16OdPuc";
 
-//buttons should be made from my topics array dynamically.
 makingButtons();
-
-$(".gif-btn").on("click", function() {
-  var searchText = $(this).attr("data-name");
-  displayGifs(searchText);
-});
 
 function makingButtons() {
   $(".buttons").empty();
@@ -21,24 +14,37 @@ function makingButtons() {
     makeBtn.text(topics[i]);
     $(".buttons").append(makeBtn);
   }
-  //   $(".button-town").append(`<button>${topics[i]}</button>`);
 }
 
-//take input of form and push it to my topics array
 $("#search").on("click", function(event) {
   event.preventDefault();
 
   var topic = $("#gifSearch").val();
-  console.log(topic);
+
   $("#gifSearch").val("");
   topics.push(topic);
   makingButtons();
+  displayGifs(topic);
 });
 
-//take the name of the button and use it to search for gifs by that name
+$(".gif-btn").on("click", function() {
+  var searchText = $(this).attr("data-name");
+  displayGifs(searchText);
+});
+
+$(".gifContainer").on("click", ".gif", function() {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
 
 function displayGifs(apple) {
-  //this returns undefined. Why?
   $(".gifContainer").empty();
 
   const queryUrl =
@@ -55,17 +61,18 @@ function displayGifs(apple) {
     const results = response.data;
     for (var i = 0; i < results.length; i++) {
       var gifDiv = $("<div>");
+      var imgStill = results[i].images.fixed_height_still.url;
+      var imgAnimate = results[i].images.fixed_height.url;
       var p = $("<p>").text("Rating: " + results[i].rating);
       var gifImage = $("<img>");
-      gifImage.attr("src", results[i].images.fixed_height.url);
+      gifImage.addClass("gif");
+      gifImage.attr("data-still", imgStill);
+      gifImage.attr("data-animate", imgAnimate);
+      gifImage.attr("src", imgStill);
+      gifImage.attr("data-state", "still");
       gifDiv.append(p);
       gifDiv.append(gifImage);
       $(".gifContainer").prepend(gifDiv);
     }
   });
 }
-
-//the already made buttons should search for gifs.
-//when I do a search, it gets added to my array and a button is made.
-//each gif should be static when loaded to the page
-//when clicked it plays, and it pauses again when clicked again.
